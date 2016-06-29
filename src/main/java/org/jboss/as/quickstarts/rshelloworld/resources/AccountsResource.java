035eb62ad72ca3ef4af94beb95841d6595a69255
Paweł <ppjzarebski@gmail.com>
2016-06-29 10:43:25 +0200

    Authorization refactoring

--- src/main/java/org/jboss/as/quickstarts/rshelloworld/resources/AccountsResource.java
+++ src/main/java/org/jboss/as/quickstarts/rshelloworld/resources/AccountsResource.java
@@ -3,24 +3,37 @@ package org.jboss.as.quickstarts.rshelloworld.resources;
 import org.jboss.as.quickstarts.rshelloworld.model.Account;
 import org.jboss.as.quickstarts.rshelloworld.model.Card;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 
-@Path("/accounts")
 public class AccountsResource {
 
-    public static ArrayList<Account> accountsList = new ArrayList<Account>() {{
+    private static ArrayList<Account> accountsList = new ArrayList<Account>() {{
         add(new Account("1", "50.0"));
         add(new Account("2", "0.0"));
         add(new Account("3", "0.0", new Card("123456", "12345")));
     }};
 
+    @Path("/accounts")
     @GET
     @Produces("application/json")
     public ArrayList<Account> getAccounts() {
         return accountsList;
+    }
+
+    @Path("/authorize")
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Account getAccount(Card card) {
+        for (Account account : accountsList) {
+            if (Objects.equals(account.getCard().getNumber(), card.getNumber()) &&
+                    Objects.equals(account.getCard().getPin(), card.getPin())) {
+                return account;
+            }
+        }
+        return accountsList.get(0);
     }
 }

28ffcdd25f3a696b8ab5b8b34121d07549d5ae2e
Paweł <ppjzarebski@gmail.com>
2016-06-29 10:34:29 +0200

    Change authorization to POST

--- src/main/java/org/jboss/as/quickstarts/rshelloworld/resources/Authorization.java
+++ src/main/java/org/jboss/as/quickstarts/rshelloworld/resources/Authorization.java
@@ -4,7 +4,7 @@ import org.jboss.as.quickstarts.rshelloworld.model.Account;
 import org.jboss.as.quickstarts.rshelloworld.model.Card;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.util.Objects;
@@ -12,7 +12,7 @@ import java.util.Objects;
 @Path("/authorize")
 public class Authorization {
 
-    @GET
+    @POST
     @Consumes("application/json")
     @Produces("application/json")
     public Account getAccount(Card card) {
